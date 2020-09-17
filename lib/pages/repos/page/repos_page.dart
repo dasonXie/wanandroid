b0700e094a28cc67dbf33e23b7436fa215e7b964
Dason <7320@orangeloan.cn>
2020-09-17 11:01:04 +0800

    repos页面添加释放判断

--- lib/pages/repos/page/repos_page.dart
+++ lib/pages/repos/page/repos_page.dart
@@ -72,8 +72,8 @@ class _ReposPageState extends State<ReposPage>
     //   print("num:$i" + projectList[i].title);
     // }
 
-    // if (mounted) {
-    setState(() {});
-    // }
+    if (mounted) {
+      setState(() {});
+    }
   }
 }
